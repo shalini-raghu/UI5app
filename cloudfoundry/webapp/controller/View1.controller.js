8a5fc3b95ededf16deeea4f659779b4d1ffc399e
shalini-raghu <shalinihraghu@gmail.com>
2019-09-19 04:47:44 +0000

    init

---
+++ cloudfoundry/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("cloudfoundry.cloudfoundry.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
